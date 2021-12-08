6ad037bec373e460005473edcd6ed81a8fa28670
K-Klear <41452954+K-Klear@users.noreply.github.com>
2021-12-08 18:21:45 +0100

    Add files via upload

---
+++ main/selector.go
@@ -0,0 +1,23 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/assets/graphics/selector.dae\"\n"
+  "material: \"/main/model.material\"\n"
+  "textures: \"/assets/graphics/selector.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 30.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ pieces/knight.go
@@ -0,0 +1,30 @@
+components {
+  id: "kinght"
+  component: "/pieces/knight.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "knight"
+  component: "/assets/graphics/knight.model"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 50.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
